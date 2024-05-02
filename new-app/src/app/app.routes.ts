097052370aad93d91897d44bde2c5311a76b67e9
Akshat <akshatiscoding@gmail.com>
2024-05-02 23:32:48 +0530

    signup

--- new-app/src/app/app.routes.ts
+++ new-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ForgetComponent } from './pages/forget/forget.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo:'login', pathMatch: 'full'
+    },
+    {
+        path: 'login', component:LoginComponent
+    },
+    {
+        path: 'signup', component:SignupComponent
+    },
+    {
+        path: 'forget', component:ForgetComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ new-app/src/app/pages/forget/forget.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-forget',
+  standalone: true,
+  imports: [],
+  templateUrl: './forget.component.html',
+  styleUrl: './forget.component.css'
+})
+
+
+export class ForgetComponent {
+
+}
